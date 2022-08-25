3189b69ac101af692e292e683f59c469ff75abfd
Lucas Gasenzer <lucasgasenzer@mac.com>
2022-08-25 14:29:30 +0200

    subdir not working yet

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ DOC_FOLDER =    "./Dokumente/"
 OUTPUT_FOLDER = "./PDF/"
 
 # retrieve all *.pdf files from DOC_FOLDER and subdirecories
-content_files = glob.glob(DOC_FOLDER+"*.pdf" , recursive=True)
+content_files = glob.glob(DOC_FOLDER+"**/*.pdf" , recursive=True)
+
+print(content_files)
 
 # iterate files and merge
 for content_file in content_files:
@@ -34,6 +36,8 @@ for content_file in content_files:
         page.show_pdf_page(page.rect, page_front, pno=0, keep_proportion=True, overlay=True, oc=0, rotate=0, clip=None)
 
     
+    # does not work with subdirectories, need to be created first or use flat file structure
+
     # output file name and dir
     result_file = content_file.replace(DOC_FOLDER, OUTPUT_FOLDER)
 
