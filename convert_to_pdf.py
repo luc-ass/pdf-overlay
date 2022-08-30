5a4d12c5543fdefb1d716a80bb06ecc59ebfefec
themorlan <github@b7hb.de>
2022-08-30 11:35:25 +0200

    introduce convert_to_pdf.py

---
+++ convert_to_pdf.py
@@ -0,0 +1,27 @@
+import comtypes.client
+import os
+
+word_PDF_format = 17
+
+DOC_FOLDER = "Dokumente/"
+
+
+def scantree(path):
+    """Recursively yield DirEntry objects for given directory."""
+    for entry in os.scandir(path):
+        if entry.is_dir(follow_symlinks=False):
+            yield from scantree(entry.path)
+        else:
+            yield entry
+
+
+input_file_list = [file.path for file in scantree(DOC_FOLDER) if file.name.lower().endswith(".docx")]
+file_list = [(file_path, file_path.replace("docx", "pdf")) for file_path in input_file_list]
+
+for input_file, output_file in file_list:
+    word = comtypes.client.CreateObject("Word.Application")
+    doc = word.Documents.Open(input_file)
+    doc.SaveAs(output_file, FileFormat=word_PDF_format)
+    doc.Close()
+
+word.Quit()
