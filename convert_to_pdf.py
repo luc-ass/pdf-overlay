1aae1d9865a99626591e142d7b30532a2877be88
themorlan <github@b7hb.de>
2022-09-12 11:24:54 +0200

    update to catch uppercase file suffixes

--- convert_to_pdf.py
+++ convert_to_pdf.py
@@ -8,7 +8,7 @@ DOC_FOLDER = "./Dokumente/"
 
 
 def convert_to_pdf(doc_folder: str) -> None:
-    file_list = [(str(file_path.absolute()), str(file_path.with_suffix(".pdf").absolute())) for file_path in pathlib.Path(doc_folder).glob('**/*.docx')]
+    file_list = [(str(file_path.absolute()), str(file_path.with_suffix(".pdf").absolute())) for file_path in pathlib.Path(doc_folder).glob('**/*.[dD][oO][cC][xX]')]
 
     word = comtypes.client.CreateObject("Word.Application")
     word.Visible = False
