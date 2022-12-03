321110040711ac3e7b83826b002cc8697bd6301f
themorlan <github@b7hb.de>
2022-12-03 21:03:36 +0100

    save time by comparing file hashes before converting file to .pdf

--- convert_to_pdf.py
+++ convert_to_pdf.py
@@ -1,6 +1,8 @@
 import comtypes.client
 import os
 import pathlib
+import json
+import hashlib
 
 word_PDF_format = 17
 
@@ -13,12 +15,26 @@ def convert_to_pdf(doc_folder: str) -> None:
     word = comtypes.client.CreateObject("Word.Application")
     word.Visible = False
 
+    curr_files = {}
+    file_db_exists = os.path.exists(f"{doc_folder}/file_hashes.json")
+    if file_db_exists:
+        with open("file_hashes.json", "r") as file:
+            _prev_files = json.load(file)
+
     for input_file, output_file in file_list:
+        checksum = hashlib.md5(open(input_file, "rb").read()).hexdigest()
+        curr_files[input_file] = checksum
+        if file_db_exists and os.path.exists(output_file):
+            if _prev_files[input_file] == checksum:
+                continue
         doc = word.Documents.Open(input_file)
         doc.SaveAs(output_file, FileFormat=word_PDF_format)
         doc.Close()
 
     word.Quit()
+
+    with open("file_hashes.json", "w") as file:
+        json.dump(curr_files, file, sort_keys=True, indent=4)
 
 
 if __name__ == "__main__":
